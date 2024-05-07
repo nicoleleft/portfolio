4334b679f09ffa33f89edb7259a60b58057b0464
Nicole Wright <wrightnicole2@yahoo.com>
2024-05-06 22:20:49 -0400

    attempt 4?

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,9 @@ export default function About() {
     return (
         <>
             <div className="background-image">
-                <img className="hero-text" src='../src/assets/hero_text.png'></img>
+                <div className="hero-text"></div>
                 <div className='pic-with-caption'>
-                    <img className="profile-pic" src='../src/assets/contact_pic.jpeg'></img>
+                    <div className="profile-pic"><img /></div>
                     <h2 className='about-description'> 
                         I am a web and applications developer with a passion for both design and code :)
                     </h2>
