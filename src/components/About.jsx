f2ff5da465d74a1b447867a2fb13b71e2c603b06
nicoleleft <61879192+nicoleleft@users.noreply.github.com>
2025-04-07 13:03:06 -0400

    text correction

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,8 +12,7 @@ export default function About() {
                     </div>
                     <p className='about-description'>
                         I am a skilled web and applications developer with a strong passion for both design and programming. 
-                        I hold a Bachelor's degree in Computer Science from the University of Central Florida, class of 2023, and
-                        am currently pursuing my Master's Degree in Digital Forensics.
+                        I hold a Bachelor's degree in Computer Science from the University of Central Florida, class of 2023.
                         <br/> 
                         <br/> 
                         Although I initially enrolled as a Forensic Chemistry major, I discovered my true interests lay elsewhere 
