6b9a9be37e231402aaeea9feb3e542c1b8ba6613
nicoleleft <61879192+nicoleleft@users.noreply.github.com>
2025-08-11 14:25:37 -0400

    fix broken pdf link

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,7 +7,7 @@ export default function Resume() {
       <div id="resume">
         <h2>resume</h2>
         <embed
-          src="/src/assets/resume_NicoleEstelleWright_2.pdf"
+          src="/resume_NicoleEstelleWright_2.pdf"
           width="60%"
           height="600px"
           type="application/pdf"
